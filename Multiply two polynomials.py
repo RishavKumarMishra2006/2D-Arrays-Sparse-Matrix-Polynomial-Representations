ae1eb48c72b060d7ce6a19ebb155abd1fe58d7fc
Rishav_Kumar_Mishra <rishavkumarmishra001@gmail.com>
2025-09-01 11:57:27 +0530

    Create Multiply two polynomials.py

---
+++ Multiply two polynomials.py
@@ -0,0 +1,9 @@
+def multiply_polynomials(poly1, poly2):
+    result = [0] * (len(poly1) + len(poly2) - 1)
+    for i in range(len(poly1)):
+        for j in range(len(poly2)):
+            result[i+j] += poly1[i] * poly2[j]
+    return result
+
+# Example
+print(multiply_polynomials([1, 2, 3], [4, 5]))  # [4, 13, 22, 15]
